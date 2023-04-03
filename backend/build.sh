787c85a8d226355ffe493aff5621581c2337fbfb
Jose Luis Peña te Winkel <josepetew@gmail.com>
2023-04-03 17:02:23 +0200

    update

--- backend/build.sh
+++ backend/build.sh
@@ -2,27 +2,9 @@
 # exit on error
 
 set -o errexit
-#----------------------------------------
-# Build frontend
-#----------------------------------------
-cd frontend && npm i && npm run build
 
-cd ..
-
-#----------------------------------------
-# Setup backend
-#----------------------------------------
 poetry install
 
 python manage.py collectstatic --no-input
 
 python manage.py migrate
-
-#----------------------------------------
-# Create user if it doesn't exist
-#----------------------------------------
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input
-fi
-
